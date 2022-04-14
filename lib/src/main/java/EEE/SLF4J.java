b67ebde2a2a27138aa489c29cea0e89140c4dc73
dchih <dchih@DESKTOP-9I3O2BE.cdsafi.com>
2022-04-14 11:13:24 +0800

    54654

---
+++ lib/src/main/java/EEE/SLF4J.java
@@ -0,0 +1,13 @@
+package EEE;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SLF4J {
+  private static Logger LOGGER = LoggerFactory.getLogger(SLF4J.class);
+
+  public static void main(String[] args) {
+    LOGGER.info("Hello World");
+    //System.out.println("Hello World");
+  }
+}
